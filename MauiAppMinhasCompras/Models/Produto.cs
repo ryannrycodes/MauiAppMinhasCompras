3720d62033ccb4002544e7c5897bcc9a626577a7
ryannrycodes <ry444n.yt@gmail.com>
2026-03-01 15:17:04 -0300

    Construção da Classe para fazer CRUD no SQL e correção do Produto.cs

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -1,28 +1,12 @@
 ﻿using SQLite;
-
 namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
-        string _descricao;
-
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao
-        {
-            get => _descricao;
-            set
-            {
-                if (value == null)
-                {
-                    throw new Exception("Por favor, preencha a descrição");
-                }
-
-                _descricao = value;
-            }
-        }
+        public string Descricao { get; set; }
         public double Quantidade { get; set; }
         public double Preco { get; set; }
-        public double Total { get => Quantidade * Preco; }
-    } // Fecha classe
-} // Fecha Namespace
+    }
+}   
