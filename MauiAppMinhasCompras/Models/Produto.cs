4886c3c20e636cb0ee65e41d1f4cf69c0951d2e9
ryannrycodes <ry444n.yt@gmail.com>
2026-03-01 14:37:51 -0300

    Add Models e troca da tela inicial

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite;
 
 namespace MauiAppMinhasCompras.Models
 {
-    internal class Produto
+    public class Produto
     {
-    }
-}
+        string _descricao;
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+
+                _descricao = value;
+            }
+        }
+        public double Quantidade { get; set; }
+        public double Preco { get; set; }
+        public double Total { get => Quantidade * Preco; }
+    } // Fecha classe
+} // Fecha Namespace
